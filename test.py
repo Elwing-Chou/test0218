ea5fc848b04684beb5b01d33ef52853106beb884
Elwing-Chou <birdfan8814@gmail.com>
2020-02-18 21:17:01 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-print(3)
+import pandas as pd
+from buzzutil import get_page
+
+NONCE = "ff3b45d52b"
+save = {
+    "title":[],
+    "url":[]
+}
+for i in range(10):
+    page = i + 1
+    print("頁數:", page)
+    result = get_page(page, NONCE)
+    if result == None:
+        break
+    else:
+        for (t, url) in result:
+            save["title"].append(t)
+            save["url"].append(url)
+df = pd.DataFrame(save, columns=["title", "url"])
+df.to_csv("buzz.csv", encoding="utf-8", index=False)
